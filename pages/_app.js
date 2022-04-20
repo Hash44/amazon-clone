aed9ab1e52133eda7254301cd88842a4a5a95210
Hashim <hashimdev1@gmail.com>
2022-04-20 22:50:12 +0300

    set up emotion style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
   return <Component {...pageProps} />
 }
 

786d1c6ed618795f955935abaf8a14f492f540e4
Hashim <hashimdev1@gmail.com>
2022-04-22 02:02:22 +0300

    Connect to Sanity

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import '../styles/globals.css'
-import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
+import "../styles/globals.css";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 
-const clientSideEmotionCache = createCache({ key: 'css' });
+const clientSideEmotionCache = createCache({ key: "css" });
 
-function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache,}) {
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
   return (
     <CacheProvider value={emotionCache}>
       <Component {...pageProps} />
@@ -12,4 +16,4 @@ function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache,}) 
   );
 }
 
-export default MyApp
+export default MyApp;
